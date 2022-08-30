51d43cf519ad7b8c8ec08f2cfab1ab1784766e34
Pushpendra Singh <govindtop1@gmail.com>
2022-08-30 16:21:49 +0530

    react_assignment_06

---
+++ src/Pages/Contact.js
@@ -0,0 +1,11 @@
+import { Navbar } from "../component/Navbar";
+import '../App.css';
+
+export function Contact(){
+    return(
+        <div className="Contact">
+          <Navbar/>
+            <h1>Contact us !!</h1>
+        </div>
+    )
+}
---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+import { Navbar } from '../component/Navbar';
+import '../App.css';
+export function Home(){
+   return(
+       <div className="Home">
+        <Navbar/>
+           <h1>Welcome to Home Page !!</h1>
+       </div>
+   )
+}
